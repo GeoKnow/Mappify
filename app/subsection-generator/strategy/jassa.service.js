f90bf175c2d405d7f6a26cb125eb1e189813ded8
Daniel Keil <daniel.keil@me.com>
2015-03-17 01:24:03 +0100

    fixed template path issue

--- app/subsection-generator/strategy/jassa.service.js
+++ app/subsection-generator/strategy/jassa.service.js
@@ -11,7 +11,7 @@
 
         var service = this;
 
-        var baseUrl = '/template/jassa/';
+        var baseUrl = 'template/jassa/';
 
         var templateFileSet = [{
                 templateValueProviderKey: 'app',
